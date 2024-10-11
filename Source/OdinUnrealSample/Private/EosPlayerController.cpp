e8a584bc6182a608f84da4b0afd936bbd52b527a
Stephen <stephen.fuhrmann@outlook.de>
2024-10-11 13:15:25 +0200

    Revert "remove old login routine to avoid compilation errors"
    This reverts commit b5dba6a66690b77bf3339fc1deb5f9e48164fe5d.

--- Source/OdinUnrealSample/Private/EosPlayerController.cpp
+++ Source/OdinUnrealSample/Private/EosPlayerController.cpp
@@ -31,72 +31,72 @@ void AEosPlayerController::Login()
 	It will bind a delegate to handle the callback event once login call succeeeds or fails.
 	All functions that access the OSS will have this structure: 1-Get OSS interface, 2-Bind delegate for callback and 3-Call OSS interface function (which will call the correspongin EOS OSS function)
 	*/
-	//IOnlineSubsystem* Subsystem = Online::GetSubsystem(GetWorld());
-	//IOnlineIdentityPtr Identity = Subsystem->GetIdentityInterface(); // This is the generic OSS interface that will access the EOS OSS.
+	IOnlineSubsystem* Subsystem = Online::GetSubsystem(GetWorld());
+	IOnlineIdentityPtr Identity = Subsystem->GetIdentityInterface(); // This is the generic OSS interface that will access the EOS OSS.
 
-	//// If you're logged in, don't try to login again.
-	//// This can happen if your player travels to a dedicated server or different maps as BeginPlay() will be called each time.
+	// If you're logged in, don't try to login again.
+	// This can happen if your player travels to a dedicated server or different maps as BeginPlay() will be called each time.
 
-	//FUniqueNetIdPtr NetId = Identity->GetUniquePlayerId(0);
+	FUniqueNetIdPtr NetId = Identity->GetUniquePlayerId(0);
 
-	//if (NetId != nullptr && Identity->GetLoginStatus(0) == ELoginStatus::LoggedIn)
-	//{
-	//	return;
-	//}
+	if (NetId != nullptr && Identity->GetLoginStatus(0) == ELoginStatus::LoggedIn)
+	{
+		return;
+	}
 
-	///* This binds a delegate so we can run our function when the callback completes. 0 represents the player number.
-	//You should parametrize this Login function and pass the parameter here for splitscreen.
-	//*/
-	//LoginDelegateHandle =
-	//	Identity->AddOnLoginCompleteDelegate_Handle(
-	//		0,
-	//		FOnLoginCompleteDelegate::CreateUObject(
-	//			this,
-	//			&ThisClass::HandleLoginCompleted));
+	/* This binds a delegate so we can run our function when the callback completes. 0 represents the player number.
+	You should parametrize this Login function and pass the parameter here for splitscreen.
+	*/
+	LoginDelegateHandle =
+		Identity->AddOnLoginCompleteDelegate_Handle(
+			0,
+			FOnLoginCompleteDelegate::CreateUObject(
+				this,
+				&ThisClass::HandleLoginCompleted));
 
-	//// Grab command line parameters. If empty call hardcoded login function - Hardcoded login function useful for Play In Editor. 
-	//FString AuthType;
-	//FParse::Value(FCommandLine::Get(), TEXT("AUTH_TYPE="), AuthType);
+	// Grab command line parameters. If empty call hardcoded login function - Hardcoded login function useful for Play In Editor. 
+	FString AuthType;
+	FParse::Value(FCommandLine::Get(), TEXT("AUTH_TYPE="), AuthType);
 
-	//if (!AuthType.IsEmpty()) //If parameter is NOT empty we can autologin.
-	//{
-	//	/*
-	//	In most situations you will want to automatically log a player in using the parameters passed via CLI.
-	//	For example, using the exchange code for the Epic Games Store.
-	//	*/
-	//	UE_LOG(LogTemp, Log, TEXT("Logging into EOS...")); // Log to the UE logs that we are trying to log in. 
+	if (!AuthType.IsEmpty()) //If parameter is NOT empty we can autologin.
+	{
+		/*
+		In most situations you will want to automatically log a player in using the parameters passed via CLI.
+		For example, using the exchange code for the Epic Games Store.
+		*/
+		UE_LOG(LogTemp, Log, TEXT("Logging into EOS...")); // Log to the UE logs that we are trying to log in. 
 
-	//	if (!Identity->AutoLogin(0))
-	//	{
-	//		UE_LOG(LogTemp, Warning, TEXT("Failed to login... ")); // Log to the UE logs that we are trying to log in.
-	//		// Clear our handle and reset the delegate.
-	//		Identity->ClearOnLoginCompleteDelegate_Handle(0, LoginDelegateHandle);
-	//		LoginDelegateHandle.Reset();
-	//	}
-	//}
-	//else
-	//{
-	//	/*
-	//	Fallback if the CLI parameters are empty.Useful for PIE.
-	//	The type here could be developer if using the DevAuthTool, ExchangeCode if the game is launched via the Epic Games Launcher, etc...
-	//	*/
-	//	FOnlineAccountCredentials Credentials("persistentauth", "", "");
+		if (!Identity->AutoLogin(0))
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Failed to login... ")); // Log to the UE logs that we are trying to log in.
+			// Clear our handle and reset the delegate.
+			Identity->ClearOnLoginCompleteDelegate_Handle(0, LoginDelegateHandle);
+			LoginDelegateHandle.Reset();
+		}
+	}
+	else
+	{
+		/*
+		Fallback if the CLI parameters are empty.Useful for PIE.
+		The type here could be developer if using the DevAuthTool, ExchangeCode if the game is launched via the Epic Games Launcher, etc...
+		*/
+		FOnlineAccountCredentials Credentials("persistentauth", "", "");
 
-	//	UE_LOG(LogTemp, Log, TEXT("Logging into EOS with persistentauth...")); // Log to the UE logs that we are trying to log in. 
+		UE_LOG(LogTemp, Log, TEXT("Logging into EOS with persistentauth...")); // Log to the UE logs that we are trying to log in. 
 
-	//	if (!Identity->Login(0, Credentials))
-	//	{
-	//		UE_LOG(LogTemp, Warning, TEXT("Failed to login with persistentauth, trying accountportal... ")); // Log to the UE logs that we are trying to log in.
-	//		FOnlineAccountCredentials Credentials2("accountportal", "", "");
-	//		
-	//		if (!Identity->Login(0, Credentials2))
-	//		{
-	//			// Clear our handle and reset the delegate.
-	//			Identity->ClearOnLoginCompleteDelegate_Handle(0, LoginDelegateHandle);
-	//			LoginDelegateHandle.Reset();
-	//		}
-	//	}
-	//}
+		if (!Identity->Login(0, Credentials))
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Failed to login with persistentauth, trying accountportal... ")); // Log to the UE logs that we are trying to log in.
+			FOnlineAccountCredentials Credentials2("accountportal", "", "");
+			
+			if (!Identity->Login(0, Credentials2))
+			{
+				// Clear our handle and reset the delegate.
+				Identity->ClearOnLoginCompleteDelegate_Handle(0, LoginDelegateHandle);
+				LoginDelegateHandle.Reset();
+			}
+		}
+	}
 }
 
 
